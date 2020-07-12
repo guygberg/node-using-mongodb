32b6138ed34627ed5ca99da514948db2ddf6f566
Guy Grinberg <Guy.Grinberg@skyboxsecurity.com>
2020-07-12 20:18:41 +0300

    module 3

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require( 'body-parser');
 const mongoose = require('mongoose'); 
+const routes = require('./src/routes')
 
 mongoose.connect('mongodb://localhost/productsdb', { useNewUrlParser: true,useUnifiedTopology :true  })
 
 mongoose.Promise = global.Promise;
 const app = express();
 const PORT = 4000;
+
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +17,8 @@ app.use(bodyParser.json());
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
 );
+
+routes(app);
 
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`)
